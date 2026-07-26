03755914ceeb42f56bd38e5edfa664eaca35d7ef
Ganesh Guntamadugu <ganeshgana868@gmail.com>
2026-07-26 21:29:11 +0530

    Updated something in Terraform

---
+++ ec2/ec2-instance.tf
@@ -0,0 +1,37 @@
+#To create Security group
+resource "aws_security_group" "sg_terraform" {
+    name = "Allow SSH"
+    description = "Allow SSH protocal 22"
+
+    #inbound rule
+    ingress{
+        description = "Allow port number 22"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    #outbound rule
+    egress{
+        description = "Allow all"
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        Name = "Allow_SSH"
+    }
+}
+
+#To create ec2-instance
+resource "aws_instance" "instance_terraform" {
+    ami = "ami-0220d79f3f480ecf5"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [aws_security_group.sg_terraform.id]
+    tags = {
+        Name = "terraform"
+    }
+}
