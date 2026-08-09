165eaeb0abe6b789b52d2a484bfd468a873af333
Ganesh Guntamadugu <ganeshgana868@gmail.com>
2026-08-09 17:14:24 +0530

    Updated something

---
+++ 08-remote-state/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "gangs-remote-state"
+    key = "remote-state-demo"
+    region = "us-east-1"
+    dynamodb_table = "gangs_locking"
+  }
+  
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
